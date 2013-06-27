fdc9f40031e1bf52466e60fd856154e1085ba7b0
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-06-26 22:12:23 -0700

    welcome to the fab cube!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fab::Application.config.secret_token = '639e8cf01370750e252ea6b366e31d3026802099117ea662c803da68880788aa91baf8fee3983386ca1390551c95fd522a60ec861a527faf61237331c735010c'
